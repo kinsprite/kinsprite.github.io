9ac67fbe5bb538c26ec83952406b49240c05776d
qinzq <qinzq@haikuotech.com>
2014-05-15 09:38:44 +0800

    _run_server.bat hide set path echo.

--- _run_server.bat
+++ _run_server.bat
@@ -1,3 +1,4 @@
+@echo off
 chcp 65001
 
 set ruby_path=D:\apps\Ruby200-x64\bin
@@ -5,4 +6,6 @@ set python_path=D:\apps\Python27_x64
 
 set path_old=%path%
 set path=%ruby_path%;%python_path%;%python_path%\Scripts;%path%;
+@echo on
+
 jekyll serve
